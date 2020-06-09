266d7ea37a62e2ae1ee7af9dac2ad794fab4dbb4
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-06-09 22:27:31 +0200

    Add some C++14 modifications for MSVC

--- lib/src/element/port.cpp
+++ lib/src/element/port.cpp
@@ -54,7 +54,7 @@ namespace cycfi { namespace elements
    view_limits vport_element::limits(basic_context const& ctx) const
    {
       view_limits e_limits = subject().limits(ctx);
-      return {{e_limits.min.x, min_port_size }, e_limits.max };
+      return view_limits{ point(e_limits.min.x, min_port_size), e_limits.max };
    }
 
    void vport_element::prepare_subject(context& ctx)
